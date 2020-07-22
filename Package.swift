4fd8146c5a2995087f3939ca0dc7bd4ad074626b
Reda Lemeden <git@redalemeden.com>
2020-07-22 13:44:39 +0200

    Fix dependency graph

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.2
 import PackageDescription
 
 let package = Package(
@@ -10,25 +10,22 @@ let package = Package(
     .library(name: "DecodableRoutes", targets: ["DecodableRoutes"]),
   ],
   dependencies: [
-    .package(url: "https://github.com/SwiftGlide/glide.git", .branch("master")),
-    .package(url: "https://github.com/SwiftGlide/query-string-coder.git", .branch("master")),
-    .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.0.0"),
-    .package(url: "https://github.com/apple/swift-nio", from: "2.12.0")
+    .package(url: "https://github.com/SwiftGlide/glide.git", from: "0.0.9"),
+    .package(url: "https://github.com/SwiftGlide/query-string-coder.git", from: "0.0.3"),
+    .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.0.0")
   ],
   targets: [
     .target(
       name: "DecodableRoutes",
       dependencies: [
         .product(name: "Glide", package: "glide"),
-        .product(name: "QueryStringCoder", package: "query-string-coder"),
-
-    ]),
+        .product(name: "QueryStringCoder", package: "query-string-coder")
+      ]),
     .testTarget(
       name: "DecodableRoutesTests",
       dependencies: [
         .target(name: "DecodableRoutes"),
-        .product(name: "AsyncHTTPClient", package: "async-http-client"),
-        .product(name: "NIO", package: "swift-nio")
-    ]),
+        .product(name: "AsyncHTTPClient", package: "async-http-client")
+      ]),
   ]
 )
