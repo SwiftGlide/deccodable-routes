f8eae03e06b936381201731bd4929618ec33d8eb
Reda Lemeden <git@redalemeden.com>
2020-07-22 12:49:04 +0200

    Update package names

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
     .library(name: "DecodableRoutes", targets: ["DecodableRoutes"]),
   ],
   dependencies: [
-    .package(name: "Glide", url: "https://github.com/SwiftGlide/glide.git", .branch("master")),
-    .package(name: "QueryStringCoder", url: "https://github.com/SwiftGlide/query-string-coder.git", .branch("master")),
+    .package(url: "https://github.com/SwiftGlide/glide.git", .branch("master")),
+    .package(url: "https://github.com/SwiftGlide/query-string-coder.git", .branch("master")),
     .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.0.0"),
     .package(url: "https://github.com/apple/swift-nio", from: "2.12.0")
   ],
@@ -19,8 +19,8 @@ let package = Package(
     .target(
       name: "DecodableRoutes",
       dependencies: [
-        .product(name: "Glide", package: "Glide"),
-        .product(name: "QueryStringCoder", package: "QueryStringCoder"),
+        .product(name: "Glide", package: "glide"),
+        .product(name: "QueryStringCoder", package: "query-string-coder"),
 
     ]),
     .testTarget(
